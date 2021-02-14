ec692ad71ae4b86a052a51ba5f2b2c297c7da723
Miguel <migueltafmart@gmail.com>
2021-02-14 16:59:01 +0100

    Enviroment variables issue

--- API/models/apiKeys.js
+++ API/models/apiKeys.js
@@ -1,12 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = process.env.URI;
 const ObjectId = require("mongodb").ObjectID;
 const emailRegEx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const idRegEx = /^[0-9a-fA-F]{24}$/;
 async function connection() {
     let client;
     try {
-        client = await MongoClient(url, {
+        client = await MongoClient(process.env.URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
